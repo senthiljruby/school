c07d25b8e15f9400d136c298f683b7b448fc2b9a
Senthil Kumar <senthil@linuxmint.linuxmint-domain>
2014-11-15 23:31:42 -0500

    Adding Birthday reminders on dashboard

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,5 +7,18 @@ class HomeController < ApplicationController
     @last_teacher = TeachingStaff.last
     @non_teachers = NonTeachingStaff.all
     @last_non_teacher = NonTeachingStaff.last
+
+    #Birthday
+    @birthday_name = []
+    @birthday_phone_number = []
+    @birthday_role = []
+    today = Date.today.strftime("%d/%m/%Y")
+    student_birthdays = Student.find_all_by_dob(today)
+    student_birthdays.each do |student_birthday|
+      @birthday_name << student_birthday.name
+      @birthday_phone_number << student_birthday.phone_number
+      @birthday_role << "Class : " + student_birthday.standard + " - " + student_birthday.section
+    end
+    teachers_birthdays = TeachingStaff.find_all_by_date_of_birth(today)    
   end
 end
