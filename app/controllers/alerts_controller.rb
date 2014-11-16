d51615ab008b835b54e045594205055ba57f3631
Senthil Kumar <senthil@linuxmint.linuxmint-domain>
2014-11-15 22:58:39 -0500

    Adding Noticeboard SMS App

--- app/controllers/alerts_controller.rb
+++ app/controllers/alerts_controller.rb
@@ -20,6 +20,58 @@ class AlertsController < ApplicationController
   end
 
   def send_multi_message
-    render :text => "TEST"
+    @teachers = params[:teachers]
+    @student_standards = params[:student_standards]
+    @student_sections = params[:student_sections]
+    @non_teachers = params[:non_teachers]
+
+    mobile_numbers = []
+    
+    unless @teachers.blank?
+      @teachers.each do |teacher|
+        teaching_staff = TeachingStaff.find_by_full_name(teacher)
+        unless teaching_staff.blank?
+          mobile_numbers << teaching_staff.mobile
+        end
+      end
+    end
+
+    unless @student_standards.blank?
+      @student_standards.each do |standard|
+        student_standard_lists = Student.find_all_by_standard(standard)
+        unless student_standard_lists.blank?
+          student_standard_lists.each do |student_standard_list|
+            mobile_numbers << student_standard_list.phone_number
+          end
+        end
+      end
+    end
+
+    unless @student_sections.blank?
+      @student_sections.each do |section|
+        student_section_lists = Student.find_all_by_standard(section)
+        unless student_section_lists.blank?
+          student_section_lists.each do |student_section_list|
+            mobile_numbers << student_section_list.phone_number
+          end
+        end
+      end
+    end
+
+    unless @non_teachers.blank?
+      @non_teachers.each do |non_teacher|
+        non_teaching_staff = NonTeachingStaff.find_by_full_name(non_teacher)
+        unless non_teaching_staff.blank?
+          mobile_numbers << non_teaching_staff.mobile
+        end
+      end
+    end
+    setting = Settings.first
+    waysms = WaySms::SmsSender.new(setting.username, setting.password)
+    mobile_numbers.uniq!.each do |mobile_number|  
+      waysms.send(mobile_number, "#{params[:messages]} - by vetri School")
+    end
+    flash[:notice] = "Message Send Successfully"
+    redirect_to alerts_noticeboard_path
   end
 end
