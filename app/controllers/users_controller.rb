88a5ea09fe948ef23e8e9bce38b83bfdbe661144
Senthil Kumar <senthil@linuxmint.linuxmint-domain>
2014-11-16 18:04:40 -0500

    Adding Navbar active and adding http authentication

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
  layout "sessions"
+ before_filter :basic_authenticate
  def new
     @user = User.new
   end
@@ -10,6 +11,14 @@ class UsersController < ApplicationController
       redirect_to root_url, :notice => "Signed up!"
     else
       render "new"
+    end
+  end
+
+  protected
+
+  def basic_authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "admin" && password == "$enthil@1617"
     end
   end
 end
